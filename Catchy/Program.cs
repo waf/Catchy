7d623f590567606324828ff2ee01f709f554bc18
Will Fuqua <wafuqua@gmail.com>
2018-12-08 11:18:55 +0700

    Add project files.

---
+++ Catchy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Catchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
